28b0b9cbbdfeddddd03c3a22ba5a72f6f23a2822
Harinder Singh <harmiglany@yahoo.com>
2022-10-03 13:31:59 -0700

    commit tree f3b9ec667a9af0a1f06378f942b2726c915db96a

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        //console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+
+    return gifts;
+}
+
+wrapGifts(gifts);
+
+function writeCards(names, event) {
+    let cards = []
+    for (let a = 0; a < names.length; a++) {
+        cards.push(`Thank you, ${names[a]}, for the wonderful ${event} gift!`)
+    }
+    console.log(cards);
+    return cards;
+}
+
+function countDown(num) {
+    //let countDown = 0;
+    while (num >= 0) {
+        console.log(num);
+        num--
+    } 
+}
